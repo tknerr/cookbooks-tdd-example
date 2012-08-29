d1bb9b9024dbecc90c647fcb2cb63edca5f0433b
tkn <tkn@zuehlke.com>
2012-08-29 11:10:36 +0200

    use fauxhai in default_spec to simulate a Chef run on Ubuntu and CentOS and the match the file contents

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,9 +1,24 @@
 require 'faster_require'
 require 'chefspec'
+require 'fauxhai'
 
 describe 'tdd-example::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'tdd-example::default' }
-  it 'should create "/tmp/foo"' do
-    chef_run.should create_file '/tmp/foo'
+
+  context "on Ubuntu" do
+    before { Fauxhai.mock(platform: 'ubuntu') }
+    let (:chef_run) { ChefSpec::ChefRunner.new.converge 'tdd-example::default' }
+
+    it 'should create "/tmp/foo" telling that it\'s running on Ubuntu' do
+      chef_run.should create_file_with_content '/tmp/foo', 'hey, I\'m running on ubuntu!'
+    end
+  end
+
+  context "on CentOS" do
+    before { Fauxhai.mock(platform: 'centos') }
+    let (:chef_run) { ChefSpec::ChefRunner.new.converge 'tdd-example::default' }
+
+    it 'should create "/tmp/foo" telling that it\'s running on CentOS' do
+      chef_run.should create_file_with_content '/tmp/foo', 'hey, I\'m running on centos!'
+    end
   end
 end
