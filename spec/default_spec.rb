075d59a33d3614e7d613ed15ac787577ae40db26
tkn <tkn@zuehlke.com>
2012-08-29 10:48:22 +0200

    create failing chefspec test

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,8 +1,9 @@
+require 'faster_require'
 require 'chefspec'
 
 describe 'tdd-example::default' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'tdd-example::default' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
+  it 'should create "/tmp/foo"' do
+    chef_run.should create_file '/tmp/foo'
   end
 end
